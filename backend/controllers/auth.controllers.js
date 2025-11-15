bce23ea0dbb99e9a814aea46df5c0d6516fb904a
Kishan Verma <kishan7777verma@gmail.com>
2025-11-15 20:18:27 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -34,8 +34,8 @@ export const signUp = async (req, res) => {
         res.cookie("token", token,{
             httpOnly:true,
             maxAge:10*365*24*60*60*1000,
-            secure:false,
-            sameSite:"Strict"
+            secure:true,
+            sameSite:"none"
         })
 
         return res.status(201).json(user)
@@ -65,8 +65,8 @@ export const signIn = async (req, res) => {
         res.cookie("token", token,{
             httpOnly:true,
             maxAge:10*365*24*60*60*1000,
-            secure:false,
-            sameSite:"Strict"
+            secure:true,
+            sameSite:"none"
         })
 
         return res.status(201).json(user)
