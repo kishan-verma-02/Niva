15e6b710957a20113bd19af110478ff1e58333ee
Kishan Verma <kishan7777verma@gmail.com>
2025-11-15 20:16:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ dotenv.config()
 const port = process.env.PORT || 3000
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://niva-1.onrender.com",
     credentials: true
 }))
 app.use(express.json())
